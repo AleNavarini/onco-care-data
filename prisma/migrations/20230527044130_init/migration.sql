60641b8051d3ce41fe179ad0cbc13b718d92b654
Alejandro Navarini <alejandronavarini@gmail.com>
2023-05-27 01:43:15 -0300

    Add prisma and cockroachdb

---
+++ prisma/migrations/20230527044130_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "email" STRING NOT NULL,
+    "name" STRING,
+    "password" STRING NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");

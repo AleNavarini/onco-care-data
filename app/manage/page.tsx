6c7cfbba7fd1b36e1f419edfa752b255c4e414d9
Alejandro Navarini <alejandronavarini@gmail.com>
2023-05-30 20:42:54 -0300

    Create diseases

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,10 +1,44 @@
 "use client"
-import { Typography } from "@mui/joy";
+import DiseasesDashboard from "@/components/DiseasesDashboard";
+import { LinearProgress, Sheet } from "@mui/joy";
+import useSWR from "swr";
+
+const getDiseases = async (url: string) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
 
 export default function ManagePage() {
+    const {
+        data: diseasesData,
+        isLoading: diseasesLoading,
+        error: diseasesError } = useSWR(`/api/diseases`, getDiseases, { refreshInterval: 5000 });
+
+
+    if (diseasesError) return <h1>Ha ocurrido un error ... </h1>
+    if (diseasesLoading) return <LinearProgress />
     return (
-        <Typography>
-            Manage page
-        </Typography>
+        <Sheet
+            sx={{
+                display: 'flex'
+            }}
+        >
+            <Sheet
+                sx={{
+                    width: {
+                        sm: '100%',
+                        md: '30%'
+                    }
+                }}
+            >
+
+                Enfermedades
+                <DiseasesDashboard diseases={diseasesData.diseases} />
+            </Sheet>
+            <Sheet>Tratamientos</Sheet>
+            <Sheet>Estudios</Sheet>
+
+        </Sheet>
     )
 }
---
+++ prisma/migrations/20230530231745_add_diseases_and_risk_factors/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Disease" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "name" STRING NOT NULL,
+    "patientId" INT8,
+
+    CONSTRAINT "Disease_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RiskFactor" (
+    "id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "name" STRING NOT NULL,
+    "value" STRING,
+    "diseaseId" INT8,
+    "patientId" INT8,
+
+    CONSTRAINT "RiskFactor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Disease_patientId_key" ON "Disease"("patientId");
+
+-- AddForeignKey
+ALTER TABLE "Disease" ADD CONSTRAINT "Disease_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RiskFactor" ADD CONSTRAINT "RiskFactor_diseaseId_fkey" FOREIGN KEY ("diseaseId") REFERENCES "Disease"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RiskFactor" ADD CONSTRAINT "RiskFactor_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE SET NULL ON UPDATE CASCADE;

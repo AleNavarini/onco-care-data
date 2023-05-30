0b4c070d6ec05ac138cc3b656dba02c72c0fb1ac
Alejandro Navarini <alejandronavarini@gmail.com>
2023-05-30 00:14:48 -0300

    Adapted responsive

--- components/Common/Accordion.tsx
+++ components/Common/Accordion.tsx
@@ -19,7 +19,6 @@ export default function Accordion({ title, children }: AccordionProps) {
     return (
         <Sheet
             sx={{
-                width: '30dvw',
                 borderRadius: 'md'
             }}
         >

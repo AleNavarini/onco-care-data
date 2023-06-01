6d354eff6a4f0e666d9fddc0ac703d0ef47860fe
Alejandro Navarini <alejandronavarini@gmail.com>
2023-05-31 23:46:36 -0300

    Add study types and attributes to management

--- app/api/study-types/[id]/route.ts
+++ app/api/study-types/[id]/route.ts
@@ -1,6 +1,27 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
+export async function GET(request: Request, context: { params: any }) {
+  const id = context.params.id;
+  try {
+    const studyType = await prisma.studyType.findFirst({
+      where: {
+        id: BigInt(id),
+      },
+      include: {
+        attributes: true,
+      },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      studyType,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
+}
 export async function PUT(request: Request, context: { params: any }) {
   const id = context.params.id;
   const { name } = await request.json();

775680d6657d54b6f3588cf6e26b524239d295da
Alejandro Navarini <alejandronavarini@gmail.com>
2023-10-17 01:26:19 -0300

    Improve loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import useSWR from 'swr';
-import { Box, LinearProgress, Typography } from '@mui/joy'
+import { Box, LinearProgress, Sheet, Typography } from '@mui/joy'
 import PatientsDashboard from '@/components/Dashboards/PatientsDashboard';
 
 const getPatients = async () => {
@@ -15,15 +15,22 @@ const getPatients = async () => {
 export default function Home() {
   const { data, error } = useSWR('/api/patients?detailed=true', getPatients, { refreshInterval: 1000 });
 
-  let content = <LinearProgress sx={{ marginY: 10 }} />
+  let content = <Sheet sx={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '80vh'
+  }}><LinearProgress /></Sheet>
   if (data) content = <PatientsDashboard patients={data.patients} />
   if (error) content = <Typography level='h3'>Ha habido un error...</Typography>
 
   return (
     <Box>
-      <Typography level="h2" sx={{ ml: 5 }}>
-        Pacientes
-      </Typography>
+      <Sheet sx={{ height: '10vh' }}>
+        <Typography level="h1" sx={{ ml: 5 }}>
+          Pacientes
+        </Typography>
+      </Sheet>
       {content}
     </Box>
   )

56cc031500c7bcc381fe7211f3cd5db2ce46cc20
Alejandro Navarini <alejandronavarini@gmail.com>
2023-10-29 01:43:39 -0300

    Refactor manages

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,27 +1,16 @@
 'use client';
 import AddDiseaseButton from '@/components/Dashboards/Diseases/AddDiseaseButton';
 import DiseasesDashboard from '@/components/Dashboards/Diseases/DiseasesDashboard';
-import StudyTypesDasboard from '@/components/Dashboards/StudyTypesDasboard';
+import AddStudyTypeButton from '@/components/Dashboards/StudyTypes/AddStudyTypeButton';
+import StudyTypesDashboard from '@/components/Dashboards/StudyTypes/StudyTypesDashboard';
+
 import AddTreatmentTypeButton from '@/components/Dashboards/TreatmentTypes/AddTreatmentTypeButton';
 import TreatmentTypesDasboard from '@/components/Dashboards/TreatmentTypes/TreatmentTypesDashboard';
-import fetcher from '@/utils/fetcher';
 import { Box, CircularProgress, Sheet, Typography } from '@mui/joy';
 import { Suspense } from 'react';
-import useSWR from 'swr';
 
-const fetchData = async (url: string) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
 
 export default function ManagePage() {
-  const { data: studyTypesData } = useSWR(`/api/study-types`, fetcher, {
-    suspense: true,
-  });
-
-  const studyTypes = studyTypesData?.studyTypes;
-
   return (
     <Sheet
       sx={{
@@ -73,13 +62,20 @@ export default function ManagePage() {
             sm: '100%',
             md: '50%',
           },
+          display: 'flex',
+          flexDirection: 'column',
+          rowGap: 2
         }}
       >
         <Suspense fallback={<CircularProgress />}>
-          <Typography mb={1} level="h3">
-            Estudios
-          </Typography>
-          <StudyTypesDasboard studyTypes={studyTypes} />
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}>
+            <Typography level="h3">Estudios</Typography>
+            <AddStudyTypeButton />
+          </Box>
+          <StudyTypesDashboard />
         </Suspense>
       </Sheet>
     </Sheet>

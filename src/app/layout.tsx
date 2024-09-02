7215f7911e21b926db4298f43c631af1eef47bce
Alejandro <ale.navarini99@gmail.com>
2024-09-01 23:42:59 -0300

    Change layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import '@/lib/big-int-extensions';
 import { Public_Sans } from 'next/font/google';
 import ClientCssVarsProvider from '@/components/providers/client-css-vars-provider';
 import NextAuthProvider from '@/components/providers/next-auth-provider';
+import FirstSidebar from '@/components/common/first-sidebar';
+import Header from '@/components/common/header';
+import MainContent from '@/components/common/main-content';
 const publicSans = Public_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({
@@ -20,7 +23,9 @@ export default function RootLayout({
             <title>Onco-Care-Data</title>
           </head>
           <body className={`${publicSans.className} body-class`}>
-            {children}
+            <Header />
+            <FirstSidebar />
+            <MainContent>{children}</MainContent>
           </body>
         </html>
       </ClientCssVarsProvider>

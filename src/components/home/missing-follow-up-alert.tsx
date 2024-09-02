0cb5efdefd4f44ac39a3e3612f5a0d08b71b0b86
Alejandro <ale.navarini99@gmail.com>
2024-09-01 23:43:08 -0300

    format

--- src/components/home/missing-follow-up-alert.tsx
+++ src/components/home/missing-follow-up-alert.tsx
@@ -2,21 +2,21 @@ import { Alert, Typography } from '@mui/joy';
 import { Patient } from '@prisma/client';
 
 interface MissingFollowUpAlertProps {
-    patient: Patient;
+  patient: Patient;
 }
 
 const MissingFollowUpAlert: React.FC<MissingFollowUpAlertProps> = ({
-    patient,
+  patient,
 }) => {
-    return (
-        <Alert variant="soft" color="warning" sx={{ mb: 2 }}>
-            <Typography fontWeight="bold">
-                {patient.name} necesita un seguimiento
-            </Typography>
-            {patient.email && <Typography>Email: {patient.email}</Typography>}
-            {patient.phone && <Typography>Telefono: {patient.phone}</Typography>}
-        </Alert>
-    );
+  return (
+    <Alert variant="soft" color="warning" sx={{ mb: 2 }}>
+      <Typography fontWeight="bold">
+        {patient.name} necesita un seguimiento
+      </Typography>
+      {patient.email && <Typography>Email: {patient.email}</Typography>}
+      {patient.phone && <Typography>Telefono: {patient.phone}</Typography>}
+    </Alert>
+  );
 };
 
 export default MissingFollowUpAlert;
--- src/components/home/missing-follow-up.tsx
+++ src/components/home/missing-follow-up.tsx
@@ -10,39 +10,39 @@ import fetcher from '@/utils/fetcher';
 import MissingFollowUpAlert from './missing-follow-up-alert';
 
 const MissingFollowUps: React.FC = () => {
-    const { data, error, isLoading } = useSWR<Patient[]>(
-        '/api/patients-missing-follow-up',
-        fetcher,
-    );
+  const { data, error, isLoading } = useSWR<Patient[]>(
+    '/api/patients-missing-follow-up',
+    fetcher,
+  );
 
-    if (isLoading) {
-        return (
-            <Typography>Cargando pacientes con seguimiento pendiente...</Typography>
-        );
-    }
-
-    if (error) {
-        return (
-            <Typography color="danger">
-                Error al cargar pacientes: {error.message}
-            </Typography>
-        );
-    }
-
+  if (isLoading) {
     return (
-        <Box>
-            {data && data.length > 0 && (
-                <Box sx={{ mb: 4 }}>
-                    {data.map((patient: Patient) => (
-                        <MissingFollowUpAlert
-                            key={patient.id.toString()}
-                            patient={patient}
-                        />
-                    ))}
-                </Box>
-            )}
-        </Box>
+      <Typography>Cargando pacientes con seguimiento pendiente...</Typography>
     );
+  }
+
+  if (error) {
+    return (
+      <Typography color="danger">
+        Error al cargar pacientes: {error.message}
+      </Typography>
+    );
+  }
+
+  return (
+    <Box>
+      {data && data.length > 0 && (
+        <Box sx={{ mb: 4 }}>
+          {data.map((patient: Patient) => (
+            <MissingFollowUpAlert
+              key={patient.id.toString()}
+              patient={patient}
+            />
+          ))}
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default MissingFollowUps;

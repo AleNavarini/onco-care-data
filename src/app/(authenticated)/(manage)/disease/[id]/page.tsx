d1bc2656583ae7e58373afbe97aefebfbb6165ce
Alejandro <ale.navarini99@gmail.com>
2024-09-04 00:04:15 -0300

    Server side fetch disease name

--- src/app/(authenticated)/(manage)/disease/[id]/page.tsx
+++ src/app/(authenticated)/(manage)/disease/[id]/page.tsx
@@ -1,18 +1,27 @@
-'use client';
-
 import RiskFactorsDashboard from '@/components/dashboards/risk-factors/risk-factor-dashboard';
+import prisma from '@/lib/prisma';
 
 interface Props {
-  params: {
-    id: string;
-  };
+  params: { id: string };
 }
 
-export default function DiseasePage({ params }: Props) {
+export default async function DiseasePage({ params }: Props) {
+  const { id } = params;
+  const disease = await prisma.disease.findUnique({
+    where: {
+      id: BigInt(id),
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
   return (
     <div className="flex flex-col justify-center items-center gap-6 py-10">
-      <h2 className="text-2xl font-bold">Factores de riesgo</h2>
-      <RiskFactorsDashboard diseaseId={params.id} />
+      <h2 className="text-2xl font-bold">
+        Factores de riesgo - {disease.name}
+      </h2>
+      <RiskFactorsDashboard diseaseId={disease.id.toString()} />
     </div>
   );
 }

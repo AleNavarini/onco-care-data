256281d26859df6f5e090bd985e9acd701ba23a5
Alejandro <ale.navarini99@gmail.com>
2024-09-04 02:26:40 -0300

    wip treatments

--- src/app/(authenticated)/(detail)/[patientId]/treatments/page.tsx
+++ src/app/(authenticated)/(detail)/[patientId]/treatments/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import TreatmentsWidget from '@/components/dashboards/treatments/treatments-widget';
+import AddButton from '@/components/common/add-button';
+import StudiesDashboard from '@/components/dashboards/studies/studies-dashboard';
+import TreatmentForm from '@/components/forms/treatment-form';
 
 interface TreatmentsPageProps {
   params: {
@@ -10,5 +12,18 @@ interface TreatmentsPageProps {
 
 export default function TreatmentsPage({ params }: TreatmentsPageProps) {
   const { patientId } = params;
-  return <TreatmentsWidget width={100} patientId={patientId} />;
+  return (
+    <div
+      className='flex flex-col gap-4 items-end w-full'
+    >
+      <div className='flex justify-between'>
+        <p>Tratamientos</p>
+        <AddButton
+          text="Crear Tratamiento"
+          form={<TreatmentForm patientId={patientId} />}
+        />
+      </div>
+      {/* <StudiesDashboard patientId={patientId} /> */}
+    </div>
+  )
 }

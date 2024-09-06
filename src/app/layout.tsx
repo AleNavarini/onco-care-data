c8c0188203dc0885654d721362d1547e1844f5e5
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-09-06 11:11:00 -0300

    Remove MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import NextAuthProvider from '@/components/providers/next-auth-provider';
 import './globals.css';
 import { Public_Sans } from 'next/font/google';
-import ClientCssVarsProvider from '@/components/providers/client-css-vars-provider';
 const publicSans = Public_Sans({ subsets: ['latin'] });
 
 export default function RootLayout({
@@ -13,14 +12,12 @@ export default function RootLayout({
 }) {
   return (
     <NextAuthProvider>
-      <ClientCssVarsProvider>
-        <html lang="en">
-          <head>
-            <title>Onco-Care-Data</title>
-          </head>
-          <body className={`${publicSans.className}`}>{children}</body>
-        </html>
-      </ClientCssVarsProvider>
+      <html lang="en">
+        <head>
+          <title>Onco-Care-Data</title>
+        </head>
+        <body className={`${publicSans.className}`}>{children}</body>
+      </html>
     </NextAuthProvider>
   );
 }

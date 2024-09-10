9ba70b87fad6106d110c9e5f259537621509b880
Alejandro Navarini <agi_anavarin@contractor.indeed.com>
2024-09-10 11:31:01 -0300

    Add prisma reference where missing

--- src/app/api/v1/diseases/route.ts
+++ src/app/api/v1/diseases/route.ts
@@ -1,6 +1,7 @@
 import '@/lib/big-int-extensions';
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+
 export async function GET(request: Request) {
   try {
     const diseases = await prisma.disease.findMany({});
--- src/app/api/v2/treatment-types/[id]/attributes/route.ts
+++ src/app/api/v2/treatment-types/[id]/attributes/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
 
 export async function GET(request: NextRequest, context: { params: any }) {
   const id = context.params.id;

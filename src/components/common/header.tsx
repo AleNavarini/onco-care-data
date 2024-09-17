df72b29e97297526c4d6fc6e39584785c0121ace
Alejandro <ale.navarini99@gmail.com>
2024-09-16 23:44:33 -0300

    Make header big z

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -7,13 +7,13 @@ interface HeaderProps {
 
 export default function Header({ toggleSidebar }: HeaderProps) {
   return (
-    <div className="absolute top-0 right-0 flex items-end justify-end w-full px-4 py-2 md:hidden">
+    <div className="absolute top-0 right-0 flex items-end justify-end w-full px-4 py-2 md:hidden z-50">
       <button
         onClick={toggleSidebar}
         aria-label="Toggle Sidebar"
         className="p-2 border rounded-md"
       >
-        <Bars3Icon className="w-6 h-6" />
+        <Bars3Icon className="w-6 h-6 bg-background" />
       </button>
     </div>
   );
